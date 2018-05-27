aa6fa94cd7c98cff1e627b4fce1a2a82ebf69607
bernatixer <bernat.torres.bellido@gmail.com>
2018-05-27 16:32:34 +0200

    Final changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var value;
 var config = {
   apiKey: "AIzaSyBE4vUV63UFSyQ1iI5TJ8OwsLqiBdcd_E4",
   authDomain: "hackreviewer-9f759.firebaseapp.com",
@@ -8,6 +9,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var cards = [];
+
 var query = firebase.database().ref();
   query.on("value", function(dbsnapshot) {
       dbsnapshot.forEach(function(postsnapshot) {
@@ -15,24 +18,85 @@ var query = firebase.database().ref();
         var url = postsnapshot.child("info/url").val();
         var text = postsnapshot.child("info/text").val();
         var sentiment = parseFloat(postsnapshot.child("info/sentiment").val());
-
-        posarCard(titol, sentiment, url, text);
+        
+        cards.push({titol: titol, sentiment: sentiment, url: url, text: text});
       })
+      for (var i=0; i<cards.length; ++i) {
+        var card = cards[i];
+        posarCard(card.titol, card.sentiment, card.url, card.text);
+      }
+      cards.sort(function(b, a){
+        return a.sentiment - b.sentiment;
+      });
    })
 
+function good() {
+  $('#linea').html('');
+  for (var i=0; i<cards.length; ++i) {
+    var card = cards[i];
+    if (card.sentiment > 0.5) posarCard(card.titol, card.sentiment, card.url, card.text);
+  }
+}
+
+function neutral() {
+  $('#linea').html('');
+  for (var i=0; i<cards.length; ++i) {
+    var card = cards[i];
+    if (card.sentiment > -0.1 && card.sentiment < 0.3) posarCard(card.titol, card.sentiment, card.url, card.text);
+  }
+}
+
+function bad() {
+  cards.sort(function(b, a){
+    return b.sentiment - a.sentiment;
+  });
+  $('#linea').html('');
+  for (var i=0; i<cards.length; ++i) {
+    var card = cards[i];
+    if (card.sentiment < 0.0) posarCard(card.titol, card.sentiment, card.url, card.text);
+  }
+}
+
+var rangeSlider = function(){
+  var slider = $('.range-slider'),
+      range = $('.range-slider__range'),
+      value = $('.range-slider__value');
+    
+  slider.each(function(){
+
+    value.each(function(){
+      var value = $(this).prev().attr('value');
+      $(this).html(value);
+    });
+
+    range.on('input', function(){
+      $(this).next(value).html(this.value);
+      console.log(this.value );
+      if(this.value>60){
+        good();}
+      else if (this.value<40) bad();
+      else neutral()
+    });
+  });
+};
+
+rangeSlider();
+
 function posarCard(titol, sentiment, url, text) {
-  console.log(sentiment)
   var red = parseInt(128-sentiment*127);
   red += 30;
   if (red > 255) red = 255;
   var green = parseInt(128+sentiment*127);
-  $('#linea').append('<div class="col-sm-4">'+
-    '<div class="card" style="background-color:rgb('+red+', '+green+', 0); max-width: 18rem;">'+
-        '<div class="card-header"><h3>'+titol+'</h3></div>'+
-        '<div class="card-body">'+
+  var num = parseFloat(parseInt((sentiment + 1)*50))/10;
+  $('#linea').append('<div class="col-sm-4" align="center">'+
+    '<a href='+url+'> <div class="card" style="background-color:rgb('+red+', '+green+', 0); max-width: 25rem;">'+
+        '<div class="card-header"><h3>'+titol+'</h3></div> </a>'+
+        '<a class="bodycard" href='+url+'> <div class="card-body">'+
           '<p class="card-text">'+text+'</p><hr/>'+
-          '<p class="card-text">'+sentiment+'</p>'+
+          '<p class="card-text" style = "text-align: center">'+num+'</p>'+
         '</div>'+
       '</div>'+
-  '</div>')
+  '</div> </a>')
 }
+
+
